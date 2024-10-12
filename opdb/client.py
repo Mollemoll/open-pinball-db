6e0e6edfdfd04d08b0be3acecfb772a93e7a5a8a
Mollemoll <jonas.molander@gmail.com>
2024-10-12 21:15:30 +0200

    add headers to client

--- opdb/client.py
+++ opdb/client.py
@@ -1,3 +1,8 @@
 class Client:
     def __init__(self):
         self.base_url = "https://opdb.org/api"
+        self.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "User-Agent": "python opdb client"
+        }
--- tests/test_client.py
+++ tests/test_client.py
@@ -5,3 +5,10 @@ class TestClient(unittest.TestCase):
     def test_initialization(self):
         self.client = opdb.Client()
         self.assertEqual(self.client.base_url, "https://opdb.org/api")
+        self.assertEqual(
+            self.client.headers,
+            {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "User-Agent": "python opdb client"
+            })
