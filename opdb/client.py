2480aabd8b930a0f4b975be5341c41c72d455f6d
Mollemoll <jonas.molander@gmail.com>
2024-10-12 21:16:35 +0200

    client can get the public changelog

--- opdb/client.py
+++ opdb/client.py
@@ -1,3 +1,5 @@
+import requests
+
 class Client:
     def __init__(self):
         self.base_url = "https://opdb.org/api"
@@ -6,3 +8,11 @@ class Client:
             "Content-Type": "application/json",
             "User-Agent": "python opdb client"
         }
+
+    def get_changelog(self):
+        return self._public_get(endpoint="changelog")
+
+    def _public_get(self, endpoint: str, params: dict = None):
+        url = f"{self.base_url}/{endpoint}"
+        response = requests.get(url, headers=self.headers, params=params)
+        return response.json()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,6 @@
 import unittest
+import requests
+import responses
 import opdb
 
 class TestClient(unittest.TestCase):
@@ -12,3 +14,38 @@ class TestClient(unittest.TestCase):
                 "Content-Type": "application/json",
                 "User-Agent": "python opdb client"
             })
+
+    @responses.activate
+    def test_get_changelog(self):
+        """ Get the changelog from the public API """
+        responses.add(
+            responses.GET,
+            'https://opdb.org/api/changelog',
+            json=[
+                {
+                    "changelog_id": 1,
+                    "opdb_id_deleted": "GrdNZ-MQo1e",
+                    "action": "move",
+                    "opdb_id_replacement": "GRveZ-MNE38",
+                    "created_at": "2018-10-19T15:06:20.000000Z",
+                    "updated_at": "2018-10-19T15:06:20.000000Z"
+                },
+            ],
+            status=200
+        )
+
+        self.client = opdb.Client()
+        self.assertEqual(
+            self.client.get_changelog(),
+            [
+                {
+                    "changelog_id": 1,
+                    "opdb_id_deleted": "GrdNZ-MQo1e",
+                    "action": "move",
+                    "opdb_id_replacement": "GRveZ-MNE38",
+                    "created_at": "2018-10-19T15:06:20.000000Z",
+                    "updated_at": "2018-10-19T15:06:20.000000Z"
+                },
+            ]
+        )
+
